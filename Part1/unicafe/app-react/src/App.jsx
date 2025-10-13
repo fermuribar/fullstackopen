d3f59d50e9dd3040f97d9f274590ae273a198007
fermuribar <fermuribar@correo.ugr.es>
2025-10-13 22:18:15 +0200

    1.9

--- Part1/unicafe/app-react/src/App.jsx
+++ Part1/unicafe/app-react/src/App.jsx
@@ -10,10 +10,6 @@ const Statistics = ({good, neutral, bad}) => {
   let total = good+neutral+bad
   let average = (good-bad)/total
   let positive = (good/total)*100
-  if (total == 0){
-    average = 0
-    positive = 0
-  }
   return(
     <div>
       <div>average {average}</div>
@@ -22,18 +18,28 @@ const Statistics = ({good, neutral, bad}) => {
   )
 }
 
+const AllDisplay = ({good, neutral, bad}) => {
+  let total = good+neutral+bad
+  if (total == 0) return <div>No feedback given</div>
+  else return(
+    <div>
+      <Display text={"good"} value={good}/>
+      <Display text={"neutral"} value={neutral}/>
+      <Display text={"bad"} value={bad}/>
+      <Display text={"all"} value={total}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+  }
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const incrementState = (state, setState) => {
-    console.log('Now ', state, ' and increment to ', state+1)
-    setState(state+1)
-  }
-  let total = good+neutral+bad
-
+  const incrementState = (state, setState) => setState(state+1)
+  
   return (
     <div>
       <Title text={"give feedback"}/>
@@ -41,11 +47,7 @@ const App = () => {
       <Button handleClick={() => incrementState(neutral,setNeutral)} text={"neutral"}/>
       <Button handleClick={() => incrementState(bad,setBad)} text={"bad"}/>
       <Title text={"statistics"}/>
-      <Display text={"good"} value={good}/>
-      <Display text={"neutral"} value={neutral}/>
-      <Display text={"bad"} value={bad}/>
-      <Display text={"all"} value={total}/>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <AllDisplay good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
