956309db4936665ceab8ab63f86090fb7de5a837
fermuribar <fermuribar@correo.ugr.es>
2025-10-14 22:35:23 +0200

    2.5

--- Part2/courseinfo/app-react/src/App.jsx
+++ Part2/courseinfo/app-react/src/App.jsx
@@ -1,26 +1,4 @@
-const Header = ({courseName}) => <h1>{courseName}</h1>
-
-const Content = ({parts}) => (
-  <div>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-  </div>
-)
-
-const Part = ({part}) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-)
-
-const Total = ({total}) => <b>Total of {total} exercises</b>
-
-const Course = ({courses}) => courses.map((course) => <div key = {course.id}>
-  <Header courseName={course.name} />
-  <Content parts={course.parts} />
-  <Total total={course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
-</div>
-)
-
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
