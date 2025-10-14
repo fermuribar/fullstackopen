63759aed5e45430685f255486455f9b3426fab10
fermuribar <fermuribar@correo.ugr.es>
2025-10-14 22:33:18 +0200

    2.4

--- Part2/courseinfo/app-react/src/App.jsx
+++ Part2/courseinfo/app-react/src/App.jsx
@@ -14,41 +14,61 @@ const Part = ({part}) => (
 
 const Total = ({total}) => <b>Total of {total} exercises</b>
 
-const Course = ({course}) => <div>
+const Course = ({courses}) => courses.map((course) => <div key = {course.id}>
   <Header courseName={course.name} />
   <Content parts={course.parts} />
   <Total total={course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
 </div>
+)
+
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return <Course courses={courses} />
 }
 
 export default App
